eac084228b3dbf1609c23764f34aa1caadce35b2
spawner999 <lore.ferrario@gmail.com>
2016-05-05 13:42:44 -0700

    add post reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export conts FETCH_POSTS = 'FETCH_POSTS';
+export const FETCH_POSTS = 'FETCH_POSTS';
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=loredev';
 
