924b310662d73de0aa11624d65feb83a4e9045c0
spawner999 <lore.ferrario@gmail.com>
2016-05-06 11:43:32 -0700

    list of post now renders on the main page

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -8,6 +8,17 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    return this.props.posts.map((post) => {
+      return(
+        <li className="list-group-item" key={post.id}>
+          <span className="pull-xs-right">{post.categories}</span>
+          <strong>{post.title}</strong>
+        </li>
+      )
+    })
+  }
+
   render() {
     return(
       <div>
@@ -16,7 +27,10 @@ class PostsIndex extends Component {
             Add A Post
           </Link>
         </div>
-        List of Posts
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
@@ -26,4 +40,8 @@ class PostsIndex extends Component {
 //   return bindActionCreators({fetchPosts}, dispatch);
 // } this can be removed and use that shortcut below. We are mapping the action to this component's props
 
-export default connect(null, {fetchPosts})(PostsIndex); //es6 syntax
+function mapStateToProps(state) {
+  return {posts: state.posts.all};
+}
+
+export default connect(mapStateToProps, {fetchPosts})(PostsIndex); //es6 syntax
