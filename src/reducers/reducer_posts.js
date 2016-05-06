ba15f75f2cf1f65c86c996d1c2e55b1db60b54e6
spawner999 <lore.ferrario@gmail.com>
2016-05-06 14:29:49 -0700

    add api call for single post

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,4 +1,5 @@
 import {FETCH_POSTS} from '../actions/index';
+import {FETCH_POST} from '../actions/index';
 
 const INITIAL_STATE = {all: [], post: null};
 
@@ -6,6 +7,8 @@ export default function(state = INITIAL_STATE, action) {
   switch(action.type) {
     case FETCH_POSTS:
       return {...state, all: action.payload.data};
+    case FETCH_POST:
+      return {...state, post: action.payload.data};
     default:
       return state;
   }
