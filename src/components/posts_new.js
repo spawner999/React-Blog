82f63a7dc5a481bb5287bb73b6e73aae8cd37bd2
spawner999 <lore.ferrario@gmail.com>
2016-05-06 10:43:47 -0700

    add validation to all form fields and some styling

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {reduxForm} from 'redux-form';
 import {createPost} from '../actions/index';
+import {Link} from 'react-router';
 
 class PostsNew extends Component {
   render() {
@@ -8,7 +9,7 @@ class PostsNew extends Component {
     return (
       <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Create a new post</h3>
-        <div className="form-group">
+        <div className={`form-group ${title.touched && title.invalid? 'has-danger' : '' }`}>
           <label>Title</label>
           <input type="text" className="form-control" {...title} />
           <div className="text-help">
@@ -16,19 +17,26 @@ class PostsNew extends Component {
           </div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${categories.touched && categories.invalid? 'has-danger' : '' }`}>
           <label>Categories</label>
           <input type="text" className="form-control" {...categories}/>
+            <div className="text-help">
+              {categories.touched ? categories.error : ''}
+            </div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${content.touched && content.invalid? 'has-danger' : '' }`}>
           <label>Content</label>
           <textarea className="form-control" {...content}/>
+            <div className="text-help">
+              {content.touched ? content.error : ''}
+            </div>
         </div>
 
         <button type="submit" className="btn btn-primary">
           Submit
         </button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     );
   }
@@ -38,7 +46,13 @@ function validate(values) {
   const errors = {};
 
   if(!values.title) {
-    errors.title = 'Enter a username';
+    errors.title = 'Enter a title';
+  }
+  if(!values.categories) {
+    errors.categories = 'Enter some categories';
+  }
+  if(!values.content) {
+    errors.content = 'Enter some content';
   }
 
   return errors;
